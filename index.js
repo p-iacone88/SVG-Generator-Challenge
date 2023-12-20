3f85fa6a9e52716c257e650410c9299868a55778
[philip iacone] <[philiacone@gmail.com]>
2023-12-20 17:51:20 -0500

    added comments to code

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
+// Inquirer imported dynamically to avoid errors
 let inquirer;
 
 import('inquirer')
   .then(module => {
     inquirer = module.default;
-
+//modules imported
     const fs = require('fs/promises');
     const { Circle, Triangle, Square } = require('./lib/shapes');
-
+// SVG class to create and render SVG elements
     class SVG {
       constructor() {
+// Initialize text and shape elements
         this.textElement = '';
         this.shapeElement = '';
       }
-    
+// Combine text and shape elements into SVG string
       render() {
         return `<svg version='1.1' xmlns='http://www.w3.org/2000/svg' width='300' height='200'>${this.shapeElement}${this.textElement}</svg>`;
       }
     
       setTextElement(text, color) {
+// Set text element with specified text and color
         this.textElement = `<text x='150' y='125' font-size='60' text-anchor='middle' fill='${color}'>${text}</text>`;
       }
     
       setShapeElement(shape) {
+// Set shape element using the provided shape object
         this.shapeElement = shape.render();
       }
     }
-    
+// Define questions for user input in an array of objects
     const questions = [
       {
         type: 'input',
@@ -53,7 +57,7 @@ import('inquirer')
         message: 'Enter the color for the text: ',
       },
     ];
-    
+// Function to write data to a file
     async function writeToFile(fileName, data) {
       console.log(`Writing [${data}] to file [${fileName}]`);
       try {
@@ -63,19 +67,20 @@ import('inquirer')
         console.error(err);
       }
     }
-    
+// Main initialization function        
     async function init() {
       console.log('Starting initialization');
       const svg_file = 'logo.svg';
     
       try {
+// Collect user input using Inquirer
         const answers = await inquirer.prompt(questions);
-    
+// Log user choices    
         console.log(`Your logo characters are: ${answers.text}`);
         console.log(`You chose ${answers.shape} as the shape type`);
         console.log(`You chose ${answers.shapeColor} for the color of the shape`);
         console.log(`You chose ${answers.textColor} as the font color`);
-    
+// Map user's shape choice to corresponding class
         const shapeTypes = {
           'square': Square,
           'circle': Circle,
@@ -86,16 +91,17 @@ import('inquirer')
         const ShapeClass = shapeTypes[selectedShapeType];
     
         if (ShapeClass) {
+// Create selected shape and set color          
           const userShape = new ShapeClass();
           console.log(`User selected ${selectedShapeType.charAt(0).toUpperCase() + selectedShapeType.slice(1)} shape`);
           userShape.setColor(answers.shapeColor);
-    
+// Create the SVG object, set elements, and render it
           const svg = new SVG();
           svg.setTextElement(answers.text, answers.textColor);
           svg.setShapeElement(userShape);
     
           const svgString = svg.render();
-    
+// Write SVG string to file    
           console.log(`Displaying shape:\n\n${svgString}`);
           console.log('Shape generation complete!');
           console.log('Writing shape to file...');
@@ -108,9 +114,9 @@ import('inquirer')
         console.error('An error occurred:', error);
       }
     }
-    
+// Start application    
     init();
   })
   .catch(error => {
-    console.error('Failed to import inquirer:', error);
+    console.error('Failed to import inquirer:', error); //display error if inquirer fails to import properly
   });
